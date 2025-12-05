fed0eb8a4a0cd25a9d23626cf74a3612dca01069
AlexandreOttmann <ottmann.alex@gmail.com>
2025-12-05 20:10:36 +0100

    added bg on absolute element

--- app/components/content/Works.vue
+++ app/components/content/Works.vue
@@ -1,17 +1,29 @@
 <template>
   <section :class="isCanvasView ? '' : 'mx-auto flex max-w-4xl flex-col p-7 mt-4  sm:mt-20'">
-    <h1 class="font-newsreader italic text-white-shadow text-center text-4xl"
-      :class="isCanvasView ? 'fixed left-1/2 -translate-x-1/2 z-50 top-20 ' : ''">
-      <slot name="title" mdc-unwrap="p" />
-    </h1>
-    <h2 class="text-center text-lg font-extralight italic text-muted"
-      :class="isCanvasView ? 'fixed left-1/2 -translate-x-1/2 z-50 top-30 ' : ''">
-      <slot name="subtitle" mdc-unwrap="p" />
-    </h2>
-    <Divider v-if="!isCanvasView" class="mb-8 mt-2" />
-    <div :class="isCanvasView ? 'fixed left-1/2 -translate-x-1/2 z-50 top-40' : 'flex justify-center mb-8'">
-      <UiViewToggle v-model="isCanvasView" />
+    <div v-if="isCanvasView"
+      class="fixed left-1/2 -translate-x-1/2 z-50 top-16 backdrop-blur-xs bg-gradient-to-b from-black/10 via-black/5 to-transparent rounded-3xl px-10 py-4 shadow-lg">
+      <h1 class="font-newsreader italic text-white-shadow text-center text-4xl">
+        <slot name="title" mdc-unwrap="p" />
+      </h1>
+      <!-- <h2 class="text-center text-lg font-extralight italic text-muted mt-2">
+        <slot name="subtitle" mdc-unwrap="p" />
+      </h2> -->
+      <div class="flex justify-center mt-6">
+        <UiViewToggle v-model="isCanvasView" />
+      </div>
     </div>
+    <template v-else>
+      <h1 class="font-newsreader italic text-white-shadow text-center text-4xl">
+        <slot name="title" mdc-unwrap="p" />
+      </h1>
+      <h2 class="text-center text-lg font-extralight italic text-muted">
+        <slot name="subtitle" mdc-unwrap="p" />
+      </h2>
+      <Divider class="mb-8 mt-2" />
+      <div class="flex justify-center mb-8">
+        <UiViewToggle v-model="isCanvasView" />
+      </div>
+    </template>
     <ProjectCanvasContainer v-if="isCanvasView" @select="openProject" :projects="projects" />
     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2" v-else>
       <Motion v-for="project, index in projects" :key="project.name" as="div" :initial="{
